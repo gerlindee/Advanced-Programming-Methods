de3d79031a5ca6ae6ce85f41a9f785d423c45e02
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:33:38 +0200

    Update IList.java

--- assignment_2/ADTs/IList.java
+++ assignment_2/ADTs/IList.java
@@ -1,10 +1,13 @@
 package ADTs;
 
 import Exceptions.EmptyContainerException;
+import Model.ProgramState;
 
 public interface IList<T> {
     void addFirst(T elem);
     boolean isEmpty();
     T remove() throws EmptyContainerException;
     String toString();
+    T get(int index);
+    T getFirst();
 }
