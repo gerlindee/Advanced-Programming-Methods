422293cdc155aaa5d834ad25a42941b26c89bced
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:44:05 +0200

    Update ProgramState.java

--- assignment_2/Model/ProgramState.java
+++ assignment_2/Model/ProgramState.java
@@ -3,16 +3,21 @@ package Model;
 import ADTs.*;
 import Model.Statements.IStatement;
 
+import java.io.BufferedReader;
+import ADTs.Tuple;
+
 public class ProgramState {
     private IStack<IStatement> exeStack;
     private IDictionary<String, Integer> symTable;
     private IList<Integer> out;
     //private IStatement originalProgram;
+    private IDictionary<Integer,Tuple<String, BufferedReader>> fileTable;
 
-    public ProgramState(IStack<IStatement> es, IDictionary<String, Integer> st, IList<Integer> ot, IStatement program) {
+    public ProgramState(IStack<IStatement> es, IDictionary<String, Integer> st, IList<Integer> ot, IStatement program, IDictionary<Integer,Tuple<String, BufferedReader>> ft) {
         this.exeStack = es;
         this.symTable = st;
         this.out = ot;
+        this.fileTable = ft;
 
         this.exeStack.push(program);
     }
@@ -29,11 +34,26 @@ public class ProgramState {
         return this.out;
     }
 
+    public IDictionary<Integer,Tuple<String, BufferedReader>> getFileTable() {
+        return this.fileTable;
+    }
+
+    public int getUniqueInt() {
+        int i = 1;
+        for(Integer inx : this.fileTable.keys()) {
+            if(inx == i)
+                i++;
+        }
+        return i;
+    }
+
     public String toString() {
-        String result = "Program state: " + '\n';
-        result = result + '\t' + "Execution stack: " + this.exeStack.toString() + '\n';
-        result = result + '\t' + "Symbol table: " + this.symTable.toString() + '\n';
-        result = result + '\t' + "Out: " +  this.out.toString() + '\n';
+        String result = "";
+        result = result + "EXECUTION STACK" + "\n" + result + "---------------" + "\n" + this.getExeStack().toString() + "\n";
+        result = result + "SYMBOL TABLE" + "\n" + "------------" + "\n" + this.getSymTable().toString() + "\n";
+        result = result + "OUT" + "\n" + "---" + "\n" + this.getOut().toString() + "\n";
+        result = result + "FILE TABLE " + "\n" + "----------" + "\n" + this.getFileTable().toString() + "\n";
+        result = result + "\n";
         return result;
     }
 
@@ -41,5 +61,6 @@ public class ProgramState {
         this.exeStack = new MyStack<>();
         this.symTable = new MyDictionary<>();
         this.out = new MyList<>();
+        this.fileTable = new MyDictionary<>();
     }
 }
