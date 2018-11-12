223eea17cfc7936c612139de5514969b54639d7f
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:36:52 +0200

    Update MyList.java

--- assignment_2/ADTs/MyList.java
+++ assignment_2/ADTs/MyList.java
@@ -11,21 +11,45 @@ public class MyList<T> implements IList<T> {
         this.list = new LinkedList<>();
     }
 
+    @Override
     public void addFirst(T elem) {
         this.list.addFirst(elem);
     }
 
+    @Override
     public boolean isEmpty() {
         return this.list.isEmpty();
     }
 
+    @Override
     public T remove() throws EmptyContainerException {
         if(this.isEmpty())
             throw new EmptyContainerException("THE LIST IS EMPTY!");
         return this.list.remove();
     }
 
+    @Override
+    public T get(int index) {
+        return this.list.get(index);
+    }
+
+    @Override
+    public T getFirst() {
+        return this.list.getFirst();
+    }
+
     public String toString() {
-        return this.list.toString();
+        StringBuilder result = new StringBuilder();
+        int size = 100;
+        result.setLength(size);
+        for(T elem : this.list) {
+            if(result.length() > size) {
+                size = 2 * size;
+                result.setLength(size);
+            }
+            result.append(elem.toString());
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
