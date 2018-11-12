d3d8ec5f006f7ec2cccef28bab4a0a0965fe8c52
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:45:55 +0200

    Update IRepo.java

--- assignment_2/Repository/IRepo.java
+++ assignment_2/Repository/IRepo.java
@@ -2,8 +2,11 @@ package Repository;
 
 import Model.ProgramState;
 
+import java.io.IOException;
+
 public interface IRepo {
     ProgramState getCrtPrg();
     boolean isEmpty();
     void add(ProgramState state);
+    void logPrgStateExec(ProgramState state) throws IOException;
 }
