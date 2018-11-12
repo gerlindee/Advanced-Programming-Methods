da344318ddf4e2fcb192be54db640a0f69fa9577
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:46:32 +0200

    Update Repository.java

--- assignment_2/Repository/Repository.java
+++ assignment_2/Repository/Repository.java
@@ -4,11 +4,18 @@ import ADTs.MyList;
 import Exceptions.EmptyContainerException;
 import Model.ProgramState;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Repository implements IRepo{
     private MyList<ProgramState> states;
+    private String logFilePath;
 
-    public Repository() {
+    public Repository(String filePath) {
         this.states = new MyList<>();
+        this.logFilePath = filePath;
     }
 
     public ProgramState getCrtPrg() {
@@ -20,6 +27,7 @@ public class Repository implements IRepo{
         return new ProgramState();
     }
 
+
     public boolean isEmpty() {
         return states.isEmpty();
     }
@@ -27,4 +35,13 @@ public class Repository implements IRepo{
     public void add(ProgramState state) {
         this.states.addFirst(state);
     }
+
+    public void logPrgStateExec(ProgramState state) throws IOException {
+        FileWriter fileName = new FileWriter(this.logFilePath, true);
+        BufferedWriter buff = new BufferedWriter(fileName);
+        PrintWriter logFile = new PrintWriter(buff);
+        logFile.println(state.toString());
+        logFile.close();
+    }
 }
+
