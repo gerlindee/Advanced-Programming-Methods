d90b3fe775495d6cca52144a1b138dbe6f573314
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-12 23:32:55 +0200

    Update and rename IDictionary to IDictionary.java

--- assignment_2/ADTs/IDictionary.java
+++ assignment_2/ADTs/IDictionary.java
@@ -1,7 +1,12 @@
 package ADTs;
 
+import java.util.Collection;
+
 public interface IDictionary<K,V> {
     void put(K key, V value);
     V get(K key);
+    V remove(K key);
+    Collection<V> values();
+    Collection<K> keys();
     String toString();
 }
