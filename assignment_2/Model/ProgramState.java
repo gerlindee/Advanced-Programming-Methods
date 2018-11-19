21946dc547d5ddf0cb8be3a7e9114de057d073f9
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-19 22:51:36 +0200

    Update ProgramState.java

--- assignment_2/Model/ProgramState.java
+++ assignment_2/Model/ProgramState.java
@@ -1,6 +1,8 @@
 package Model;
 
 import ADTs.*;
+import Heap.Heap;
+import Heap.IHeap;
 import Model.Statements.IStatement;
 
 import java.io.BufferedReader;
@@ -12,12 +14,14 @@ public class ProgramState {
     private IList<Integer> out;
     //private IStatement originalProgram;
     private IDictionary<Integer,Tuple<String, BufferedReader>> fileTable;
+    private IHeap heap;
 
-    public ProgramState(IStack<IStatement> es, IDictionary<String, Integer> st, IList<Integer> ot, IStatement program, IDictionary<Integer,Tuple<String, BufferedReader>> ft) {
+    public ProgramState(IStack<IStatement> es, IDictionary<String, Integer> st, IList<Integer> ot, IStatement program, IDictionary<Integer,Tuple<String, BufferedReader>> ft, IHeap hp) {
         this.exeStack = es;
         this.symTable = st;
         this.out = ot;
         this.fileTable = ft;
+        this.heap = hp;
 
         this.exeStack.push(program);
     }
@@ -38,6 +42,10 @@ public class ProgramState {
         return this.fileTable;
     }
 
+    public IHeap getHeap() {
+        return this.heap;
+    }
+
     public int getUniqueInt() {
         int i = 1;
         for(Integer inx : this.fileTable.keys()) {
@@ -52,7 +60,8 @@ public class ProgramState {
         result = result + "EXECUTION STACK" + "\n" + result + "---------------" + "\n" + this.getExeStack().toString() + "\n";
         result = result + "SYMBOL TABLE" + "\n" + "------------" + "\n" + this.getSymTable().toString() + "\n";
         result = result + "OUT" + "\n" + "---" + "\n" + this.getOut().toString() + "\n";
-        result = result + "FILE TABLE " + "\n" + "----------" + "\n" + this.getFileTable().toString() + "\n";
+        result = result + "FILE TABLE" + "\n" + "----------" + "\n" + this.getFileTable().toString() + "\n";
+        result = result + "HEAP" + "\n" + "----" + "\n" + this.getHeap().toString() + "\n";
         result = result + "\n";
         return result;
     }
@@ -62,5 +71,6 @@ public class ProgramState {
         this.symTable = new MyDictionary<>();
         this.out = new MyList<>();
         this.fileTable = new MyDictionary<>();
+        this.heap = new Heap();
     }
 }
