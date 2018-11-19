c9e0aa498cea414604df906b88d9b25f45954b16
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-19 22:44:02 +0200

    Heap is done

---
+++ assignment_2/Heap/Heap.java
@@ -0,0 +1,55 @@
+package Heap;
+
+import ADTs.IDictionary;
+import ADTs.MyDictionary;
+import Exceptions.HeapReadingException;
+
+import java.util.Collection;
+
+public class Heap implements IHeap {
+    private IDictionary<Integer, Integer> mappings;
+    private int location;
+
+    public Heap() {
+        this.mappings = new MyDictionary<>();
+        this.location = 1;
+    }
+
+    public int New(int value) {
+        int address = this.location;
+        this.mappings.put(address, value);
+        this.location++;
+        return address;
+    }
+
+    public int Read(int address) throws HeapReadingException {
+        boolean found = false;
+        for(int idx : this.getAddresses()) {
+            if(idx == address)
+                found = true;
+        }
+        if(!found)
+            throw new HeapReadingException();
+        return this.mappings.get(address);
+
+    }
+
+    public void Update(int address, int value) throws HeapReadingException {
+        boolean found = false;
+        for(int idx : this.getAddresses()) {
+            if(idx == address)
+                found = true;
+        }
+        if(!found)
+            throw new HeapReadingException();
+        this.mappings.put(address, value);
+    }
+
+    private Collection<Integer> getAddresses() {
+        return this.mappings.keys();
+    }
+
+    public String toString() {
+        return this.mappings.toString();
+    }
+}
