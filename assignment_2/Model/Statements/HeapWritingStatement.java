0c4e449eee1f9d4cb74c0543f4887fb0fbbd0938
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-19 22:46:02 +0200

    Heap writing and allocation done

---
+++ assignment_2/Model/Statements/HeapWritingStatement.java
@@ -0,0 +1,33 @@
+package Model.Statements;
+
+import ADTs.IDictionary;
+import Exceptions.DivisionByZeroException;
+import Exceptions.HeapReadingException;
+import Exceptions.UndefinedOperationException;
+import Exceptions.UndefinedVariableException;
+import Heap.IHeap;
+import Model.Expressions.IExpression;
+import Model.ProgramState;
+
+public class HeapWritingStatement implements IStatement {
+    private String var_name;
+    private IExpression expression;
+
+    public HeapWritingStatement(String name, IExpression exp) {
+        this.var_name = name;
+        this.expression = exp;
+    }
+
+    public ProgramState execute(ProgramState state) throws HeapReadingException, DivisionByZeroException, UndefinedVariableException, UndefinedOperationException {
+        IDictionary<String, Integer> symTable = state.getSymTable();
+        IHeap heap = state.getHeap();
+        int address = symTable.get(this.var_name);
+        int new_value = this.expression.eval(symTable, heap);
+        heap.Update(address, new_value);
+        return state;
+    }
+
+    public String toString() {
+        return "writeHeap(" + this.var_name + "," + this.expression.toString() + ")";
+    }
+}
