c66365118b60bf9812ead7237386cad17ec8fabc
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-11-20 23:02:46 +0200

    Update IHeap.java

--- assignment_2/Heap/IHeap.java
+++ assignment_2/Heap/IHeap.java
@@ -2,9 +2,13 @@ package Heap;
 
 import Exceptions.HeapReadingException;
 
+import java.util.HashMap;
+
 public interface IHeap {
     int New(int value);
     int Read(int address) throws HeapReadingException;
     void Update(int address, int value) throws HeapReadingException;
     String toString();
+    HashMap<Integer, Integer> getDictionary();
+    void setDictionary(HashMap<Integer,Integer> content);
 }
