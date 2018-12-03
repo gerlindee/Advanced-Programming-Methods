eb8140d6b566601213d1e7c7d93085821d7a88a1
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-12-03 22:28:33 +0200

    boolean expressions implemented

---
+++ assignment_2/Model/Expressions/BooleanExpression.java
@@ -0,0 +1,75 @@
+package Model.Expressions;
+
+import ADTs.IDictionary;
+import Exceptions.DivisionByZeroException;
+import Exceptions.HeapReadingException;
+import Exceptions.UndefinedOperationException;
+import Exceptions.UndefinedVariableException;
+import Heap.IHeap;
+import Model.Expressions.IExpression;
+
+public class BooleanExpression extends IExpression {
+    private IExpression left;
+    private IExpression right;
+    private String operator;
+
+    public BooleanExpression(IExpression one, String op, IExpression two) {
+        this.left = one;
+        this.right = two;
+        this.operator = op;
+    }
+
+    public int eval(IDictionary<String, Integer> table, IHeap heap) throws HeapReadingException, UndefinedVariableException, UndefinedOperationException, DivisionByZeroException {
+        int result;
+        switch (this.operator) {
+            case "<" :
+            {
+                if(this.left.eval(table,heap) < this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            case "<=" :
+            {
+                if(this.left.eval(table,heap) <= this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            case "==" :
+            {
+                if(this.left.eval(table,heap) == this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            case "!=" :
+            {
+                if(this.left.eval(table,heap) != this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            case ">" :
+            {
+                if(this.left.eval(table,heap) > this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            case ">=" :
+            {
+                if(this.left.eval(table,heap) >= this.right.eval(table,heap))
+                    result = 1;
+                else
+                    result = 0;
+            } break;
+            default: throw new UndefinedOperationException();
+        }
+        return result;
+    }
+
+    public String toString() {
+        return "(" + this.left.toString() + this.operator + this.right.toString() + ")";
+    }
+}
