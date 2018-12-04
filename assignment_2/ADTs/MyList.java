9c1789d4904b682e57c7a53dd96363f97d4a4f55
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-12-04 21:21:21 +0200

    Update MyList.java

--- assignment_2/ADTs/MyList.java
+++ assignment_2/ADTs/MyList.java
@@ -2,41 +2,52 @@ package ADTs;
 
 import Exceptions.EmptyContainerException;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
-public class MyList<T> implements IList<T> {
-    private LinkedList<T> list;
+public class MyStack<T> implements IStack<T> {
+    private Stack<T> stack;
 
-    public MyList() {
-        this.list = new LinkedList<>();
+    public MyStack(){
+        this.stack = new Stack<>();
     }
 
-    public void addFirst(T elem) {
-        this.list.addFirst(elem);
+    public void push(T elem) {
+        stack.push(elem);
     }
     
-    public boolean isEmpty() {
-        return this.list.isEmpty();
+    public T pop() throws EmptyContainerException {
+        if(this.stack.isEmpty())
+            throw new EmptyContainerException();
+        return stack.pop();
     }
 
-    public T remove() throws EmptyContainerException {
-        if(this.isEmpty())
-            throw new EmptyContainerException();
-        return this.list.remove();
+    public boolean isEmpty() {
+        return stack.empty();
     }
 
     public String toString() {
         StringBuilder result = new StringBuilder();
         int size = 100;
         result.setLength(size);
-        for(T elem : this.list) {
-            if(result.length() > size) {
+        if(this.stack.isEmpty()){
+            result.append("[ ]");
+            result.append("\n");
+            return result.toString();
+        }
+        int i = this.stack.size() - 1;
+        result.append(this.stack.get(i).toString());
+        result.append("\n");
+        i--;
+        while(i >= 0) {
+            if (result.length() > size) {
                 size = 2 * size;
                 result.setLength(size);
             }
-            result.append(elem.toString());
+            result.append(this.stack.get(i).toString());
             result.append("\n");
+            i--;
         }
         return result.toString();
+
     }
 }
