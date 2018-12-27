abd58774bb7a21ae4a5fd9d8b801742076cf36a6
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2018-12-28 00:50:02 +0200

    Add files via upload

---
+++ Toy_Language_Interpreter_C/Controller.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a7
+{
+    class Controller
+    {
+        private IRepo repository;
+
+        public Controller(IRepo repo)
+        {
+            this.repository = repo;
+        }
+
+        public bool IsEmpty()
+        {
+            return this.repository.IsEmpty();
+        }
+
+        public void AddProgram(ProgramState prg)
+        {
+            this.repository.Add(prg);
+        }
+
+        public ProgramState OneStep(ProgramState state)
+        {
+            try
+            {
+                IStatement currentStmt = state.ExeStack.Pop();
+                state = currentStmt.Execute(state);
+            }
+            catch(EmptyStackException ex2)
+            {
+                Console.WriteLine(ex2.ToString());
+            }
+            catch(DivideByZeroException ex3)
+            {
+                Console.WriteLine(ex3.Message);
+            }
+            catch(UndefinedOperationException ex4)
+            {
+                Console.WriteLine(ex4.ToString());
+            }
+            catch(UndefinedVariableException ex5)
+            {
+                Console.WriteLine(ex5.ToString());
+            }
+            catch(MissingTextReaderException ex6)
+            {
+                Console.WriteLine(ex6.ToString());
+            }
+            catch(EndOfFileException ex7)
+            {
+                Console.WriteLine(ex7.ToString());
+            }
+            return state;
+        }
+
+        public void AllStep()
+        {
+            ProgramState state = this.repository.GetCrtPrg();
+            while(!state.ExeStack.IsEmpty())
+            {
+                this.OneStep(state);
+                Console.Write(state.ToString());
+                this.repository.LogPrgStateExec(state);
+            }
+            Console.Write("\n");
+        }
+    }
+}
---
+++ Toy_Language_Interpreter_C/ProgramState.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a7
+{
+    class ProgramState
+    {
+        private IStack<IStatement> exeStack;
+        public IStack<IStatement> ExeStack
+        {
+            get
+            {
+                return this.exeStack;
+            }
+        }
+
+        private IDictionary<String, int> symTable;
+        public IDictionary<String, int> SymTable
+        {
+            get
+            {
+                return this.symTable;
+            }
+        }
+
+        private IList<int> outTable;
+        public IList<int> OutTable
+        {
+            get
+            {
+                return this.outTable;
+            }
+        }
+
+        private IDictionary<int, MyTuple<String, StreamReader>> fileTable;
+        public IDictionary<int, MyTuple<String, StreamReader>> FileTable
+        {
+            get
+            {
+                return this.fileTable;
+            }
+        }
+        
+        public ProgramState(IStack<IStatement> es, IDictionary<String, int> st, IList<int> ot, IDictionary<int, MyTuple<String, StreamReader>> ft, IStatement program)
+        {
+            this.exeStack = es;
+            this.symTable = st;
+            this.outTable = ot;
+            this.fileTable = ft;
+
+            this.exeStack.Push(program);
+        }
+
+        public int GetUniqueID()
+        {
+            int i = 1;
+            foreach (int idx in this.fileTable.Keys())
+            {
+                if (idx == i)
+                    i++;
+            }
+            return i;
+        }
+
+        public override string ToString()
+        {
+            String result = "";
+            result = result + "EXECUTION STACK" + "\n" + result + "---------------" + "\n" + this.ExeStack.ToString() + "\n";
+            result = result + "SYMBOL TABLE" + "\n" + "------------" + "\n" + this.SymTable.ToString() + "\n";
+            result = result + "OUT" + "\n" + "---" + "\n" + this.OutTable.ToString() + "\n";
+            result = result + "FILE TABLE" + "\n" + "----------" + "\n" + this.FileTable.ToString() + "\n";
+            result = result + "\n";
+            return result;
+        }
+    }
+}
