524247af954d391d2bcd66c880606063eb596fb3
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2019-01-21 13:58:39 +0200

    Create FileTableEntry.java

---
+++ Concurrent_Toy_Language_Interpreter/View/GUI/FileTableEntry.java
@@ -0,0 +1,27 @@
+package GUI;
+
+public class FileTableEntry {
+    private Integer key;
+    private String value;
+
+    public FileTableEntry(Integer key, String value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
